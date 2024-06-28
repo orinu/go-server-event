0588743cf98329a3e18ef472be558881588f8fae
“Ori <ori.nahu@gmail.com>
2024-06-28 14:20:37 +0300

    add post request for event

--- main.go
+++ main.go
@@ -4,16 +4,33 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"orinu.com/go-server/models"
 )
 
 func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run("localhost:8080")
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindBodyWithJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event Created", "event": event})
 }
